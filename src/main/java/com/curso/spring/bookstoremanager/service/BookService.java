36202d97b740fc7d0b6b3b962eb530b690fd8f33
Giulio <giuliodille@gmail.com>
2021-12-09 15:41:10 -0300

    Implementa teste unitário do método findById()

--- src/main/java/com/curso/spring/bookstoremanager/service/BookService.java
+++ src/main/java/com/curso/spring/bookstoremanager/service/BookService.java
@@ -9,7 +9,6 @@ import com.curso.spring.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class BookService {
@@ -19,11 +18,11 @@ public class BookService {
     private final BookMapper bookMapper = BookMapper.INSTANCE;
 
     @Autowired
-    public BookService(BookRepository bookRepository){
+    public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public MessageResponseDTO create(BookDTO bookDTO){
+    public MessageResponseDTO create(BookDTO bookDTO) {
         Book bookToSave = bookMapper.toModel(bookDTO);
 
         Book savedBook = bookRepository.save(bookToSave);
--- src/test/java/com/curso/spring/bookstoremanager/service/BookServiceTest.java
+++ src/test/java/com/curso/spring/bookstoremanager/service/BookServiceTest.java
@@ -10,10 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
@@ -27,13 +30,23 @@ public class BookServiceTest {
     @Test
     void whenGivenExistingIdThenReturnThisBook() throws BookNotFoundException {
         Book expectedFoundBook = BookUtils.createFakeBook();
-        Mockito.when(bookRepository.findById(expectedFoundBook.getId())).thenReturn(Optional.of(expectedFoundBook));
+        when(bookRepository.findById(expectedFoundBook.getId())).thenReturn(Optional.of(expectedFoundBook));
 
         BookDTO bookDTO = bookService.findById(expectedFoundBook.getId());
 
         Assertions.assertEquals(expectedFoundBook.getName(), bookDTO.getName());
         Assertions.assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
         Assertions.assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+
+    @Test
+    void whenGivenUnexistingIdThenNotFindThrowAnException(){
+        var invalidId = 10L;
+
+        when(bookRepository.findById(invalidId))
+                .thenReturn(Optional.ofNullable(any(Book.class)));
+
+        assertThrows(BookNotFoundException.class, () -> bookService.findById(invalidId));
     }
 
 }
